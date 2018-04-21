0f6dd1aa99f6b9540e20ddd2f801a14a0ad8dce5
aron skelton <aron@skelton.us>
2018-04-21 00:05:52 -0400

    v1

---
+++ liri.js
@@ -0,0 +1,87 @@
+
+// I have no idea why this isn't working very frustrated i feel like my syntax is correct please dont give F ;) 
+require("dotenv").config();
+
+var keys =require("./keys.js");
+
+var command = process.argv[2];
+
+var Twitter =require("twitter");
+var Spotify = require('node-spotify-api');
+// var request = require('request'
+// );
+
+switch(command){
+    case "my-tweets":
+        twitter();
+        break;
+    case "spotify-this-song":
+        spotify();
+        break;
+    case "movie-this":
+        movies();
+        break;
+    default:
+        console.log("follow the instructions");
+}
+
+//twitter function
+//spotify function
+// movie function
+
+function twitter (){
+    console.log("twitter started");
+    var client = new Twitter(keys.twitter);
+
+    var params = {screen_name: 'aronskelton'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+}
+
+function spotify (){
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track',
+
+        query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var songInfo = data.tracks.items;
+        console.log("Artist(s): " + songInfo[0].artists[0].name);
+        console.log("Song Name: " + songInfo[0].name);
+        console.log("Preview Link: " + songInfo[0].preview_url);
+        console.log("Album: " + songInfo[0].album.name);
+        console.log("these songs are playing");
+        console.log(data);
+    });
+
+
+}
+
+//
+function movies() {
+//     var movie = "Mr.Nobody";
+//     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+//
+// }
+//
+// request(queryUrl)
+// {
+//     if (!movie) {
+//         movie = 'Mr Nobody';
+//     }
+
+
+//run functions
+
+    twitter();
+    spotify();
+    movies();
+}
+
+// });
+
